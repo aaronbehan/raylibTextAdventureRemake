db01dd5ae7587144e79461ab6355410f0edab73f
Aaron Behan <aarondevelops@outlook.com>
2023-05-05 17:31:21 +0100

    word wrapping characteristics added to the output box

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 #include "include/bottomborder.h"
 #include "include/rightborder.h"
 
-
-// MODIFIED RAYLIB FUNCTION FOR ALTERED \n PROPERTIES
-void DrawTextEx2(Font font, const char *text, Vector2 position, float fontSize, float spacing, Color tint);  
-
+// MODIFIED RAYLIB FUNCTIONS FOR ALTERED \n PROPERTIES
+void DrawTextEx2(Font font, const char *text, Vector2 position, float fontSize, float spacing, Color tint); 
+static void DrawTextBoxed(Font font, const char *text, Rectangle rec, float fontSize, float spacing, bool wordWrap, Color tint);   // Draw text using font inside rectangle limits
+static void DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSize, float spacing, bool wordWrap, Color tint, int selectStart, int selectLength, Color selectTint, Color selectBackTint);    // Draw text using font inside rectangle limits with support for text selection 
 
 Texture2D PlayerAvatar;
 Texture2D WindowRightBorder;
 Texture2D WindowBottomBorder;
 
 #define MAX_INPUT_CHARS 55
-#define MAX_OUTPUT_CHARS 25
+#define MAX_OUTPUT_CHARS 150
 
 int main(void)
 {   
@@ -24,7 +24,7 @@ int main(void)
     int screenWidth = 720;
     int screenHeight = 520;
     SetConfigFlags(FLAG_WINDOW_RESIZABLE); 
-    InitWindow(screenWidth, screenHeight, "raylib [text] example - input box");
+    InitWindow(screenWidth, screenHeight, "gooey text adventure prototype");
 
     // DEFINING INPUT & OUTPUT CHARACTER LIMITS
     char userInput[MAX_INPUT_CHARS + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
@@ -33,7 +33,9 @@ int main(void)
 
     for (int i = 0; i < MAX_OUTPUT_CHARS; i++) output[i] = 'e';
     output[17] = '\0';
-    
+
+    bool wordWrap = true;
+
     // int lengthOfLine = 30;
     // for (int i = 0; i < 1000; i++) {
     //     if ((i % lengthOfLine == 0) && (i != 0)) output[i] = '\n';
@@ -45,7 +47,7 @@ int main(void)
     Rectangle inputBox = {10, screenHeight - 40, screenWidth, 40 };
     Vector2 inputBoxTextPos = {inputBox.x, inputBox.y};
     Rectangle outputBox = {25, 25, screenWidth - 200, screenHeight - 200 };
-    Vector2 outputBoxTextPos = {outputBox.x, outputBox.y};
+    // Vector2 outputBoxTextPos = {outputBox.x, outputBox.y};
     Rectangle directionBox = {10, screenHeight - 160, 40, 60};
 
     // ANOTHER DYNAMIC RECTANGLE NEEDED TO OBSCURE "OFF-SCREEN" TEXT. AND ANOTHER TO CREATE THE ILLUSION OF A BORDER ON THE RIGHT SIDE.
@@ -180,7 +182,8 @@ int main(void)
             outputBox.height = screenHeight - 200;
 
             DrawTextEx2(font, userInput, inputBoxTextPos, 33.0, 0.3, BLACK);
-            DrawTextEx2(font, output, outputBoxTextPos, 33.0, 0.3, BLACK);
+            // DrawTextEx2(font, output, outputBoxTextPos, 33.0, 0.3, BLACK);
+            DrawTextBoxed(font, output, (Rectangle){ outputBox.x + 4, outputBox.y + 4, outputBox.width - 4, outputBox.height - 4 }, 33.0f, 0.03f, wordWrap, BLACK);
 
             
             // if (letterCount < MAX_INPUT_CHARS)
@@ -191,14 +194,10 @@ int main(void)
             // else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
 
         EndDrawing();
-
-        //----------------------------------------------------------------------------------
+        
     }
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
     return 0;
 }
 
@@ -213,7 +212,6 @@ bool IsAnyKeyPressed()
 
     return keyPressed;
 }
-
 
 void appendToOutput(char *pointerToOutput, char *feedback) 
 {
@@ -261,5 +259,141 @@ void DrawTextEx2(Font font, const char *text, Vector2 position, float fontSize, 
         }
 
         i += codepointByteCount;   // Move text bytes counter to next codepoint
+    }
+}
+
+// Draw text using font inside rectangle limits
+static void DrawTextBoxed(Font font, const char *text, Rectangle rec, float fontSize, float spacing, bool wordWrap, Color tint)
+{
+    DrawTextBoxedSelectable(font, text, rec, fontSize, spacing, wordWrap, tint, 0, 0, WHITE, WHITE);
+}
+
+// Draw text using font inside rectangle limits with support for text selection
+static void DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSize, float spacing, bool wordWrap, Color tint, int selectStart, int selectLength, Color selectTint, Color selectBackTint)
+{
+    int length = TextLength(text);  // Total length in bytes of the text, scanned by codepoints in loop
+
+    float textOffsetY = 0;          // Offset between lines (on line break '\n')
+    float textOffsetX = 0.0f;       // Offset X to next character to draw
+
+    float scaleFactor = fontSize/(float)font.baseSize;     // Character rectangle scaling factor
+
+    // Word/character wrapping mechanism variables
+    enum { MEASURE_STATE = 0, DRAW_STATE = 1 };
+    int state = wordWrap? MEASURE_STATE : DRAW_STATE;
+
+    int startLine = -1;         // Index where to begin drawing (where a line begins)
+    int endLine = -1;           // Index where to stop drawing (where a line ends)
+    int lastk = -1;             // Holds last value of the character position
+
+    for (int i = 0, k = 0; i < length; i++, k++)
+    {
+        // Get next codepoint from byte string and glyph index in font
+        int codepointByteCount = 0;
+        int codepoint = GetCodepoint(&text[i], &codepointByteCount);
+        int index = GetGlyphIndex(font, codepoint);
+
+        // NOTE: Normally we exit the decoding sequence as soon as a bad byte is found (and return 0x3f)
+        // but we need to draw all of the bad bytes using the '?' symbol moving one byte
+        if (codepoint == 0x3f) codepointByteCount = 1;
+        i += (codepointByteCount - 1);
+
+        float glyphWidth = 0;
+        if (codepoint != '\n')
+        {
+            glyphWidth = (font.glyphs[index].advanceX == 0) ? font.recs[index].width*scaleFactor : font.glyphs[index].advanceX*scaleFactor;
+
+            if (i + 1 < length) glyphWidth = glyphWidth + spacing;
+        }
+
+        // NOTE: When wordWrap is ON we first measure how much of the text we can draw before going outside of the rec container
+        // We store this info in startLine and endLine, then we change states, draw the text between those two variables
+        // and change states again and again recursively until the end of the text (or until we get outside of the container).
+        // When wordWrap is OFF we don't need the measure state so we go to the drawing state immediately
+        // and begin drawing on the next line before we can get outside the container.
+        if (state == MEASURE_STATE)
+        {
+            // TODO: There are multiple types of spaces in UNICODE, maybe it's a good idea to add support for more
+            // Ref: http://jkorpela.fi/chars/spaces.html
+            if ((codepoint == ' ') || (codepoint == '\t') || (codepoint == '\n')) endLine = i;
+
+            if ((textOffsetX + glyphWidth) > rec.width)
+            {
+                endLine = (endLine < 1)? i : endLine;
+                if (i == endLine) endLine -= codepointByteCount;
+                if ((startLine + codepointByteCount) == endLine) endLine = (i - codepointByteCount);
+
+                state = !state;
+            }
+            else if ((i + 1) == length)
+            {
+                endLine = i;
+                state = !state;
+            }
+            else if (codepoint == '\n') state = !state;
+
+            if (state == DRAW_STATE)
+            {
+                textOffsetX = 0;
+                i = startLine;
+                glyphWidth = 0;
+
+                // Save character position when we switch states
+                int tmp = lastk;
+                lastk = k - 1;
+                k = tmp;
+            }
+        }
+        else
+        {
+            if (codepoint == '\n')
+            {
+                if (!wordWrap)
+                {
+                    textOffsetY += (font.baseSize + font.baseSize/100)*scaleFactor;
+                    textOffsetX = 0;
+                }
+            }
+            else
+            {
+                if (!wordWrap && ((textOffsetX + glyphWidth) > rec.width))
+                {
+                    textOffsetY += (font.baseSize + font.baseSize/100)*scaleFactor;
+                    textOffsetX = 0;
+                }
+
+                // When text overflows rectangle height limit, just stop drawing
+                if ((textOffsetY + font.baseSize*scaleFactor) > rec.height) break;
+
+                // Draw selection background
+                bool isGlyphSelected = false;
+                if ((selectStart >= 0) && (k >= selectStart) && (k < (selectStart + selectLength)))
+                {
+                    DrawRectangleRec((Rectangle){ rec.x + textOffsetX - 1, rec.y + textOffsetY, glyphWidth, (float)font.baseSize*scaleFactor }, selectBackTint);
+                    isGlyphSelected = true;
+                }
+
+                // Draw current character glyph
+                if ((codepoint != ' ') && (codepoint != '\t'))
+                {
+                    DrawTextCodepoint(font, codepoint, (Vector2){ rec.x + textOffsetX, rec.y + textOffsetY }, fontSize, isGlyphSelected? selectTint : tint);
+                }
+            }
+
+            if (wordWrap && (i == endLine))
+            {
+                textOffsetY += (font.baseSize + font.baseSize/100)*scaleFactor;
+                textOffsetX = 0;
+                startLine = endLine;
+                endLine = -1;
+                glyphWidth = 0;
+                selectStart += lastk - k;
+                k = lastk;
+
+                state = !state;
+            }
+        }
+
+        if ((textOffsetX != 0) || (codepoint != ' ')) textOffsetX += glyphWidth;  // avoid leading spaces
     }
 }
